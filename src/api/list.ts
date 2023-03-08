360b478b07d00d015ae836b19997e900bcba32ec
webguoqing <webguoqing@163.com>
2023-03-08 22:00:43 +0800

    添加注释

--- src/api/list.ts
+++ src/api/list.ts
@@ -1,10 +1,17 @@
+/*
+ * @Author: webguoqing webguoqing@163.com
+ * @Date: 2023-03-05 21:57:09
+ * @LastEditors: webguoqing webguoqing@163.com
+ * @LastEditTime: 2023-03-08 22:00:18
+ * @FilePath: \vue-pure-admin\src\api\list.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { http } from "@/utils/http";
 
 type Result = {
   success: boolean;
   data?: {
     /** 列表数据 */
-    // list
     list: Array<any>;
   };
 };
